94c7cd62ce2f7e04da2b0fcb00089e5f010bdff2
annaAzh <forarre@yandex.ru>
2024-01-22 20:46:44 +0300

    fix: fix 04-copy-directory task (delete folder)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,35 +1,27 @@
-const fs = require('fs');
 const path = require('path');
+const { readdir, mkdir, rm, copyFile } = require('fs/promises');
 
 const folderName = 'files-copy';
 const folderPath = path.resolve(__dirname, `${folderName}`);
 
-fs.mkdir(folderPath, { recursive: true }, (err) => {
-  if (err) {
-    console.log(err.message);
-  }
-});
-
-fs.promises
-  .readdir(path.resolve(__dirname, 'files'), (err, data) => {
-    if (err) {
-      console.log(err.message);
-    }
-    return data;
-  })
+rm(folderPath, { recursive: true, force: true })
+  .then(() => mkdir(folderPath, { recursive: true }))
+  .then(() =>
+    readdir(
+      path.resolve(__dirname, 'files'),
+      { withFileTypes: true },
+      (data) => {
+        return data;
+      },
+    ),
+  )
   .then((data) => {
-    for (let file in data) {
-      const fileReadPath = path.join(__dirname, 'files', `${data[file]}`);
-      const fileWritePath = path.join(__dirname, 'files-copy', `${data[file]}`);
-      fs.readFile(fileReadPath, 'utf-8', (err, data) => {
-        if (err) {
-          console.log(err.message);
-        }
-        fs.writeFile(fileWritePath, `${data}`, (err) => {
-          if (err) {
-            console.log(err.message);
-          }
-        });
-      });
-    }
-  });
+    data.forEach((file) => {
+      const fileReadPath = path.join(__dirname, 'files', `${file.name}`);
+      const fileWritePath = path.join(__dirname, 'files-copy', `${file.name}`);
+      if (file.isFile()) {
+        copyFile(fileReadPath, fileWritePath);
+      }
+    });
+  })
+  .catch((err) => console.log(err.message));
