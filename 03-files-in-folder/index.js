fca77430fea663db4175cb0d34d92178a2bc79d7
annaAzh <forarre@yandex.ru>
2024-01-22 18:16:14 +0300

    fix: fix file size

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,25 +4,29 @@ const path = require('path');
 const filePath = path.resolve(__dirname, 'secret-folder');
 
 fs.promises
-  .readdir(filePath, (err) => {
+  .readdir(filePath, { withFileTypes: true }, (err) => {
     if (err) {
       console.log(err.message);
     }
   })
   .then((stats) => {
     for (let i = 0; i < stats.length; i++) {
-      fs.stat(path.resolve(filePath, `${stats[i]}`), (err, stat) => {
-        if (err) {
-          console.log(err.message);
-        }
-        if (!stat.isDirectory()) {
-          const notes = path.join(__dirname, 'secret-folder', `${stats[i]}`);
+      if (stats[i].isFile() && !stats[i].isDirectory()) {
+        fs.stat(path.resolve(filePath, `${stats[i].name}`), (err, stat) => {
+          if (err) {
+            console.log(err.message);
+          }
+          const notes = path.join(
+            __dirname,
+            'secret-folder',
+            `${stats[i].name}`,
+          );
           const index = path.basename(notes).indexOf('.');
           const fileName = path.basename(notes).slice(0, index);
           const fileExtname = path.extname(notes).slice(1);
-          const fileSize = `${stat.size / 1024} kb`;
+          const fileSize = `${(stat.size / 1000).toFixed(3)} kb`;
           console.log(`${fileName} - ${fileExtname} - ${fileSize}`);
-        }
-      });
+        });
+      }
     }
   });
